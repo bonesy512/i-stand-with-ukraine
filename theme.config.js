2d911f18c255b3ebe0967b3a708d4cb4fa7ebc0b
Thomas Schustereit <bonesy@bonesydesign.com>
2022-03-02 01:36:29 -0600

    main2

--- theme.config.js
+++ theme.config.js
@@ -24,15 +24,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Exposing Russian State corruption with ongoing CyOps" />
+      <meta name="og:description" content="Exposing Russian State corruption with ongoing CyOps" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="twitter:image" content="https://navytimes.com/resizer/tdY9iZVtcm8fMzqmueOGBlU4Inw=/1024x0/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/mco/5MRF74JOKBAP7NJPW6I24NCY74.jfif" />
+      <meta name="twitter:site:domain" content="fuckrussianoligarchs.xyz" />
+      <meta name="twitter:url" content="fuckrussianoligarchs.xyz" />
+      <meta name="og:title" content="Fuck Russian Oligarchs" />
+      <meta name="og:image" content="https://navytimes.com/resizer/tdY9iZVtcm8fMzqmueOGBlU4Inw=/1024x0/filters:format(jpg):quality(70)/cloudfront-us-east-1.images.arcpublishing.com/mco/5MRF74JOKBAP7NJPW6I24NCY74.jfif" />
+      <meta name="apple-mobile-web-app-title" content="Fuck Russia" />
     </>
   ),
 
