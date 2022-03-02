2da86fc0430a287dbb401e99febaf5d42e4f9ddd
Thomas Schustereit <bonesy@bonesydesign.com>
2022-03-02 14:17:08 -0600

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,32 +1,25 @@
 export default {
-  path: '/',
-  titleSuffix: 'Fuck Russian Oligarchs',
+  projectLink: 'https://github.com/shuding/nextra',
+  titleSuffix: ' – #OpRussia',
   nextLinks: true,
   prevLinks: true,
   search: true,
+  customSearch: null,
   darkMode: true,
   footer: true,
-  footerText: 'Fuck Russia © Glory To Ukraine.',
+  footerText: `The Unlicense ${new Date().getFullYear()} © Bonesy512.`,
+  footerEditLink: `Support The Ukraine Army`,
   logo: (
     <>
-     <React.Fragment>
-      <span className="mr-2 font-extrabold hidden md:inline">#OpRussia #UkraineInvasion</span>
-      <span className="text-gray-600 font-normal hidden md:inline"
-        Exposing Corrupt Russian Oligarchs
-      </span>
-     </React.Fragment>
+      <svg>...</svg>
+      <span>Russian State Corruption Exposed</span>
     </>
   ),
   head: (
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="theme-color" content="#ffffff" />
+    <>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Detailing the actions to expose the Russian State corruption with ongoing CyOps from the international community." />"
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="ScanEx-Ru taken offline" />
-      <meta name="twitter:description" content="Russian State satellite facilities were hacked and the db cloned. />
-      <meta name="twitter:image" content="https://unsplash.com/photos/70Rir5vB96U" />
-
-  unstable_faviconGlyph: '🇺🇦',
+      <meta name="description" content="FRO: Exposing Corrupt Russian Oligarchs" />
+      <meta name="og:title" content="Fuck Russian Oligarchs" />
+    </>
+  ),
 }
